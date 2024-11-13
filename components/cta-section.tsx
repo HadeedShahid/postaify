4ed58db6514c83a52fbe4b17cd1a231f6cfb07cd
Hadeed Shahid <hadeed.shahid08@gmail.com>
2024-11-13 22:36:24 +0500

    added cta section

--- components/cta-section.tsx
+++ components/cta-section.tsx
@@ -1,24 +1,27 @@
 import React from "react";
 import { Vortex } from "@/components/ui/vortex";
+import { Meteors } from "./ui/meteors";
 
 export function CtaSection() {
   return (
-    <div className="w-[calc(100%-4rem)] mx-auto rounded-md  h-[30rem] overflow-hidden">
-      <Vortex className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full">
-        <h2 className="text-white text-2xl md:text-6xl font-bold text-center">
-          The hell is this?
-        </h2>
-        <p className="text-white text-sm md:text-2xl max-w-xl mt-6 text-center">
-          This is chemical burn. It&apos;ll hurt more than you&apos;ve ever been
-          burned and you&apos;ll have a scar.
-        </p>
-        <div className="flex flex-col sm:flex-row items-center gap-4 mt-6">
-          <button className="px-4 py-2 bg-blue-600 hover:bg-blue-700 transition duration-200 rounded-lg text-white shadow-[0px_2px_0px_0px_#FFFFFF40_inset]">
-            Order now
+    <div className="w-full h-80 relative mt-40">
+      <div className="relative shadow-xl bg-gray-900 border border-gray-800 px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
+        <div className="m-auto text-center">
+          <h2 className="mt-2 text-4xl font-bold tracking-tight text-primary text-white mb-4 relative z-50">
+            Choose the right plan for you
+          </h2>
+          <p className="leading-7 text-primary text-xl text-slate-500 mb-4 relative z-50">
+            We provide reliable, scalable, and customizable email solutions for
+            your business
+          </p>
+
+          <button className="border px-4 py-1 rounded-lg  border-gray-500 text-gray-300">
+            Explore
           </button>
-          <button className="px-4 py-2  text-white ">Watch trailer</button>
         </div>
-      </Vortex>
+
+        <Meteors number={50} />
+      </div>
     </div>
   );
 }
