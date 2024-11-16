92900ef66b1f3b159784b8b45221f119ac2485f8
Hadeed Shahid <hadeed.shahid08@gmail.com>
2024-11-16 08:36:38 +0500

    font size adjust responsice

--- components/hero.tsx
+++ components/hero.tsx
@@ -7,12 +7,12 @@ import { ContainerScroll } from "./ui/container-scroll-animation";
 const Hero = () => {
   return (
     <div className="flex flex-col">
-      <div className="absolute h-[90rem] w-full dark:bg-dot-white/[0.2] bg-dot-black/[0.15] -z-10 left-0" />
+      <div className="absolute h-[60rem] md:h-[90rem] w-full dark:bg-dot-white/[0.2] bg-dot-black/[0.15] -z-10 left-0" />
       <Navbar />
       <div className="flex flex-col overflow-hidden pb-20">
         <ContainerScroll
           titleComponent={
-            <div className="bg-transparent flex flex-col justify-center text-center gap-y-12 mt-20 md:mt-24 mb-20">
+            <div className="bg-transparent flex flex-col justify-center text-center gap-y-12 mt-20 md:mt-24 mb-14 md:mb-20">
               <div className="flex flex-col mx-auto gap-y-5">
                 <h1 className="title-hero">
                   <span className="bg-gradient-to-r from-pink-500 to-purple-500 text-transparent bg-clip-text">
@@ -20,7 +20,7 @@ const Hero = () => {
                   </span>
                   your Social Media with AI
                 </h1>
-                <h2 className="desc-hero max-w-2xl mx-auto">
+                <h2 className="desc-hero max-w-2xl md:max-w-2xl  mx-auto">
                   Everything AI seamlessly integrated all the modern AI
                   generation tools into one platform so that you can generate
                   content with a single click.
@@ -38,6 +38,7 @@ const Hero = () => {
             height={700}
             alt="hero-image"
             draggable={false}
+            className="w-full h-full object-contain"
           />
         </ContainerScroll>
       </div>
